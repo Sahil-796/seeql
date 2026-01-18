34bee041ff25ae52ec22dab111bc6cc180bf6882
Sahil-796 <sp0709006@gmail.com>
2026-01-18 17:35:42 +0530

    fix: sqlparser.Walk func usage

---
+++ internal/parser/columns.go
@@ -0,0 +1,9 @@
+package parser
+
+// import "vitess.io/vitess/go/vt/sqlparser"
+
+// // ExtractColumns returns table -> columns used in query
+// func ExtractColumns(stmt sqlparser.Statement) map[string][]string {
+// 	result := make(map[string]map[string]struct{})
+
+// }
--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,10 +1,30 @@
 package parser
 
-import "testing"
+import ("testing"
+	"vitess.io/vitess/go/vt/sqlparser"
+)
 
 func TestParse(t *testing.T) {
-	sql := "SELECT * FROM users"
+	sql := `
+SELECT u.name, o.amount
+FROM users u
+JOIN orders o ON u.id = o.user_id
+`
 	stmt, err := Parse(sql)
+	t.Logf("%#v", stmt)
+	
+	sqlparser.Walk(func(node sqlparser.SQLNode) (bool, error) {
+		t.Logf("VISITING NODE: %T", node)
+
+		if col, ok := node.(*sqlparser.ColName); ok {
+			t.Logf("COLUMN: %s.%s",
+				col.Qualifier.Name.String(),
+				col.Name.String(),
+			)
+		}
+		return true, nil
+}, stmt)
+
 
 	if err != nil {
 		t.Fatalf("expected no error, got %v", err)
