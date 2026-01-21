c827cc89d4a0a45a98c681fda3a37495cfc18806
Sahil-796 <sp0709006@gmail.com>
2026-01-21 23:52:51 +0530

    feat: joins extraction

--- internal/parser/joins.go
+++ internal/parser/joins.go
@@ -2,21 +2,54 @@ package parser
 
 import (
 	"vitess.io/vitess/go/vt/sqlparser"
+	"github.com/Sahil-796/seeql/internal/types"
 )
 
-func ExtractJoins(stmt sqlparser.Statement) {
+func ExtractJoins(stmt sqlparser.Statement, aliases map[string]string) []types.Join {
+	
+	joins := make([]types.Join, 0)
 	sqlparser.Walk(func(node sqlparser.SQLNode) (bool, error) {
 
 		if cmp, ok := node.(*sqlparser.ComparisonExpr); ok {
-			if cmp.Operator.ToString() != "=" { 
-				// this bizzare piece of shit uses doesnt have a clean naming convention. wtf is Parse2`
-				return true, nil
+			
+			if cmp.Operator != sqlparser.EqualOp {
+			    return true, nil
 			}
 			leftCol, ok1 := cmp.Left.(*sqlparser.ColName)
 			rightCol, ok2 := cmp.Right.(*sqlparser.ColName)
+			
+			if !ok1 || !ok2 {
+				return true, nil
+			}
+			
+			leftQualifier := leftCol.Qualifier.Name.String()
+			rightQualifier := rightCol.Qualifier.Name.String()
 
+			leftTable, okL := aliases[leftQualifier]
+			rightTable, okR := aliases[rightQualifier]
+
+			if !okL {
+				leftTable = leftQualifier
+			}
+			if !okR {
+				rightTable = rightQualifier
+			}
+
+			if leftTable == rightTable {
+				return true, nil
+			}
+
+			joins = append(joins, types.Join{
+				LeftTable:   leftTable,
+				LeftColumn:  leftCol.Name.String(),
+				RightTable:  rightTable,
+				RightColumn: rightCol.Name.String(),
+			})
+			return true, nil
 		}
 
 		return true, nil
 	}, stmt)
+	
+	return joins
 }
---
+++ internal/types/types.go
@@ -0,0 +1,8 @@
+package types
+
+type Join struct {
+	LeftTable   string
+	LeftColumn  string
+	RightTable  string
+	RightColumn string
+}
