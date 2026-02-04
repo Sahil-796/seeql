e20ff6125d20bc3def5969efd53607185f336e6d
Sahil-796 <sp0709006@gmail.com>
2026-02-04 23:24:02 +0530

    feat: execute query function

--- internal/db/execute.go
+++ internal/db/execute.go
@@ -3,8 +3,56 @@ package db
 import (
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"fmt"
 )
 
-func ExecuteQuery(db  *sql.DB, query string) {
-	
+type ExecutionResult struct {
+	Columns  []string
+	Rows     []map[string]any
+	RowCount int
 }
+
+func ExecuteQuery(sqlDB *sql.DB, query string) (*ExecutionResult, error) {
+
+	rows, err := sqlDB.Query(query)
+	// rows has .Scan .Next and .Columns functions
+	// rows is jsut a ptr to the result after sqlite executes the query
+
+	if err != nil {
+		return nil, fmt.Errorf("query execution failed: %w", err)
+	}
+
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get columns: %w", err)
+	}
+
+	var result []map[string]any
+	for rows.Next() {
+		values := make([]any, len(columns))
+		valuePtr := make([]any, len(columns))
+
+		for i := range values {
+			valuePtr[i] = &values[i]
+		}
+		
+		if err := rows.Scan(valuePtr...); err != nil {
+			return nil, fmt.Errorf("failed to scan row: %w", err)
+		}
+		
+		row := make(map[string]any)
+		for i, col := range columns {
+			row[col] = values[i]
+		}
+		result = append(result, row)
+	}
+	
+	return &ExecutionResult{
+		Columns: columns,
+		Rows: result,
+		RowCount: len(result),
+	}, nil
+}
+
