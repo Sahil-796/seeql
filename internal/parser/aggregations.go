23572ad7fd5a7cc08105e3502fde2bdf3149d69c
Sahil-796 <sp0709006@gmail.com>
2026-02-07 19:09:17 +0530

    feat: support for other than COUNT aggregate functions

--- internal/parser/aggregations.go
+++ internal/parser/aggregations.go
@@ -26,6 +26,7 @@ func ExtractAggregations(stmt sqlparser.Statement) []AggregateExpr {
 		var aggregates []AggregateExpr
 
 		// SelectExprs.Exprs means the expressions in the SELECT clause (Select name from ... <- name is an expr)
+		// check for arguments (columns or such)
 		// go doc package sqlparser.Select
 		for _, selectExpr := range v.SelectExprs.Exprs {
 			aliasedExpr, ok := selectExpr.(*sqlparser.AliasedExpr)
@@ -53,8 +54,72 @@ func ExtractAggregations(stmt sqlparser.Statement) []AggregateExpr {
 				aggregates = append(aggregates, agg)
 			}
 
+			if sum, ok := aliasedExpr.Expr.(*sqlparser.Sum); ok {
+				agg := AggregateExpr{
+					Type:  AggSum,
+					Alias: aliasedExpr.As.String(),
+				}
+				if col, ok := sum.Arg.(*sqlparser.ColName); ok {
+					agg.Column = col.Name.String()
+				}
+				aggregates = append(aggregates, agg)
+			}
+
+			if avg, ok := aliasedExpr.Expr.(*sqlparser.Avg); ok {
+				agg := AggregateExpr{
+					Type:  AggAvg,
+					Alias: aliasedExpr.As.String(),
+				}
+				if col, ok := avg.Arg.(*sqlparser.ColName); ok {
+					agg.Column = col.Name.String()
+				}
+				aggregates = append(aggregates, agg)
+			}
+
+			if min, ok := aliasedExpr.Expr.(*sqlparser.Min); ok {
+				agg := AggregateExpr{
+					Type:  AggMin,
+					Alias: aliasedExpr.As.String(),
+				}
+				if col, ok := min.Arg.(*sqlparser.ColName); ok {
+					agg.Column = col.Name.String()
+				}
+				aggregates = append(aggregates, agg)
+			}
+
+			if max, ok := aliasedExpr.Expr.(*sqlparser.Max); ok {
+				agg := AggregateExpr{
+					Type:  AggMax,
+					Alias: aliasedExpr.As.String(),
+				}
+				if col, ok := max.Arg.(*sqlparser.ColName); ok {
+					agg.Column = col.Name.String()
+				}
+				aggregates = append(aggregates, agg)
+			}
+
 		}
 		return aggregates
 	}
 	return nil
+}
+
+// if the statement contains any aggregate functions
+func HasAggregations(stmt sqlparser.Statement) bool {
+	return len(ExtractAggregations(stmt)) > 0
+}
+
+
+//returns the expected data type for an aggregation
+func GetAggregationReturnType(aggType AggregateType) string {
+	switch aggType {
+	case AggCount:
+		return "INTEGER"
+	case AggSum, AggAvg:
+		return "FLOAT"
+	case AggMin, AggMax:
+		return "TEXT"
+	default:
+		return "TEXT"
+	}
 }
